c1bf19de3697d1c36390ef787ef48e89af9c83d7
hamzeh <h.pourshabanan@live.com>
2021-09-28 13:24:21 +0330

    Paging_3

--- app/src/androidTest/java/com/hamzeh/padranews/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hamzeh/padranews/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hamzeh.pixelnews", appContext.packageName)
+        assertEquals("com.hamzeh.padranews", appContext.packageName)
     }
 }
